bc346e3883264d1f56ea8282eaa9f01b930e15c5
Tomas Lööw <tirpen@gmail.com>
2022-12-02 07:16:45 +0100

    Day2

---
+++ src/main/kotlin/aoc2022/Day2.kt
@@ -0,0 +1,102 @@
+package aoc2022
+
+import DailyProblem
+import java.io.File
+
+typealias RPSMove = Int
+
+const val ROCK = 0
+const val PAPER = 1
+const val SCISSOR = 2
+
+typealias RPSResult = Int
+
+const val WIN = 2
+const val DRAW = 0
+const val LOSE = 1
+
+/* computer - my ≅ result (mod 3) */
+/* computer - result ≅ my (mod 3) */
+
+class Day2Problem(override val inputFilePath: String) : DailyProblem {
+
+    override val number = 2
+    override val name = "Rock Paper Scissors "
+
+    private fun charToMove(c: String): RPSMove {
+        return when (c) {
+            "A" -> ROCK
+            "B" -> PAPER
+            "C" -> SCISSOR
+            "X" -> ROCK
+            "Y" -> PAPER
+            "Z" -> SCISSOR
+            else -> {
+                throw Exception()
+            }
+        }
+    }
+
+    private fun charToRes(c: String): RPSResult {
+        return when (c) {
+            "X" -> LOSE
+            "Y" -> DRAW
+            "Z" -> WIN
+            else -> {
+                throw Exception()
+            }
+        }
+    }
+
+    private fun parseFilePart1(): List<Pair<RPSMove, RPSMove>> {
+        return File(inputFilePath).readLines().map { line ->
+            val (comp, me) = line.split(" ")
+            Pair(charToMove(comp), charToMove(me))
+        }
+    }
+
+
+    private fun parseFilePart2(): List<Pair<RPSMove,RPSResult>> {
+        return File(inputFilePath).readLines().map { line ->
+            val (comp, res) = line.split(" ")
+            Pair(charToMove(comp), charToRes(res))
+        }
+    }
+
+    override fun part1(): Long {
+        return parseFilePart1().map { (comp, me) ->
+            val res = (comp - me + 3) % 3
+            scoreRes(res) + scoreMove(me)
+        }.sum().toLong()
+    }
+
+    private fun scoreRes(res: Int): Int {
+        return when(res) {
+            WIN -> 6
+            DRAW -> 3
+            LOSE -> 0
+            else -> { throw Exception("Bad result")}
+        }
+    }
+
+    private fun scoreMove(move: Int): Int {
+        return when(move) {
+            ROCK -> 1
+            PAPER -> 2
+            SCISSOR -> 3
+            else -> { throw Exception("Bad move")}
+        }
+    }
+
+
+    override fun part2(): Long {
+        return parseFilePart2().map { (comp, res) ->
+            val me = (comp - res + 3) % 3
+            scoreRes(res) + scoreMove(me)
+        }.sum().toLong()
+    }
+}
+
+val day2Problem = Day2Problem("input/aoc2022/day2.txt")
+
+
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,13 +2,14 @@ import kotlin.time.ExperimentalTime
 
 val problems = listOf(
     aoc2022.day1Problem,
+    aoc2022.day2Problem,
 
 )
 
 @OptIn(ExperimentalTime::class)
 fun main() {
     val durations = problems.map { problem ->
-        val dur = problem.runBoth(timesToRun = 100)
+        val dur = problem.runBoth(timesToRun = 1000)
         Pair(problem.number, dur)
     }.toMap()
     println("=== Timing summary ===")
