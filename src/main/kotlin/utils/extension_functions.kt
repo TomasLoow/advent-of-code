92c735e9c80410059242acc03b7ce2db671989dd
Tomas Lööw <tomas.loow@mailbox.org>
2022-12-05 20:16:11 +0100

    Tiny optimization

--- src/main/kotlin/utils/extension_functions.kt
+++ src/main/kotlin/utils/extension_functions.kt
@@ -1,13 +1,14 @@
 package utils
 
 fun String.nonEmptyLines(): List<String> {
-    return lines().filter { it.isNotEmpty() }
+    // For some reason this is faster than using isNotEmpty()
+    return lines().filter { it.length > 0 }
 }
 
-fun IntRange.containsRange(range2: IntRange) : Boolean {
+fun IntRange.containsRange(range2: IntRange): Boolean {
     return range2.start in this && range2.endInclusive in this
 }
 
 fun IntRange.intersectRange(range2: IntRange): Boolean {
-    return ! (range2.endInclusive < start || range2.start > endInclusive)
+    return !(range2.endInclusive < start || range2.start > endInclusive)
 }
