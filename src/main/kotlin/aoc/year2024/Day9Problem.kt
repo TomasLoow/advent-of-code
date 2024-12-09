442ace887becab608abf416e1285e2805c0f8bc5
Tomas Lööw <tomas.loow@sectra.com>
2024-12-09 13:11:30 +0100

    Optimize Day 9 somewhat

--- src/main/kotlin/aoc/year2024/Day9Problem.kt
+++ src/main/kotlin/aoc/year2024/Day9Problem.kt
@@ -1,11 +1,19 @@
 package aoc.year2024
 
 import DailyProblem
-import aoc.utils.emptyMutableList
 import aoc.utils.odd
 import kotlin.time.ExperimentalTime
 
-data class Block(var empty: Boolean, var fileIdx: Long?, var length: Int)
+data class Block(var fileIdx: Int, var length: Int) {
+    val empty: Boolean get() = fileIdx == -1
+    companion object {
+        fun empty(length: Int) : Block{
+            return Block(fileIdx = -1, length=length)
+        }
+    }
+}
+
+const val EMPTY = -1
 
 class Day9Problem : DailyProblem<Long>() {
 
@@ -14,24 +22,23 @@ class Day9Problem : DailyProblem<Long>() {
     override val name = "Disk Fragmenter"
 
     private var size: Long = 0
-    private lateinit var disk: Array<Long?>
+    private lateinit var disk: IntArray
     private lateinit var blocks: MutableList<Block>
-    private var numberOfFiles = 0
 
     override fun commonParts() {
         val rawData = getInputText().filter { it.isDigit() }.map { it.toString().toLong() }
         size = rawData.sum()
-        disk = Array<Long?>(size.toInt()) { null }
-        blocks = emptyMutableList<Block>()
+        disk = IntArray(size.toInt()) { EMPTY }
+        blocks = ArrayList(rawData.size)
         var diskIdx = 0
-        var fileCounter = 0L
+        var fileCounter = 0
         rawData.forEachIndexed { inputIdx, i ->
 
             if (inputIdx.odd) {
                 diskIdx += i.toInt()
-                blocks.add(Block(empty = true, fileIdx = null, length = i.toInt()))
+                blocks.add(Block.empty(length = i.toInt()))
             } else {
-                blocks.add(Block(empty = false, fileIdx = fileCounter, length = i.toInt()))
+                blocks.add(Block(fileIdx = fileCounter, length = i.toInt()))
                 repeat(i.toInt()) {
                     disk[diskIdx] = fileCounter
                     diskIdx++
@@ -43,68 +50,69 @@ class Day9Problem : DailyProblem<Long>() {
 
 
     override fun part1(): Long {
-        var leftmostEmptyIdx = disk.indexOfFirst { it == null }
-        var rightmostNonEmptyIdx = disk.indexOfLast { it != null }
+        var leftmostEmptyIdx = disk.indexOfFirst { it == EMPTY }
+        var rightmostNonEmptyIdx = disk.indexOfLast { it != EMPTY }
         while (leftmostEmptyIdx < rightmostNonEmptyIdx) {
             disk[leftmostEmptyIdx] = disk[rightmostNonEmptyIdx]
-            disk[rightmostNonEmptyIdx] = null
-            leftmostEmptyIdx = disk.indexOfFirst { it == null }
-            rightmostNonEmptyIdx = disk.indexOfLast { it != null }
+            disk[rightmostNonEmptyIdx] = EMPTY
+            while (disk[leftmostEmptyIdx] != EMPTY) {
+                leftmostEmptyIdx++
+            }
+            while (disk[rightmostNonEmptyIdx] == EMPTY) {
+                rightmostNonEmptyIdx--
+            }
         }
         return score(disk)
     }
 
-    private fun score(diskToScore: Array<Long?>): Long {
+    private fun score(diskToScore: IntArray): Long {
         return diskToScore.mapIndexed { idx, i ->
-            if (i == null) 0L else idx * i!!
+            if (i == EMPTY) 0L else idx.toLong() * i.toLong()
         }.sum()
-
     }
 
 
     override fun part2(): Long {
         val files = blocks.filter { !it.empty }.toMutableList()
+        var blockIdx = blocks.size - 1
         repeat(files.size) {
-            var fileToMove = files.removeLast()
+            val fileToMove = files.removeLast()
+            while (blocks[blockIdx] != fileToMove) {
+                blockIdx--
+            }
 
-            val blockIdx = this.blocks.indexOf(fileToMove)
             // Make current position empty. It doesn't matter if we end up with consecutive empty since we have already tried to move all blocks to the right of it.
-            this.blocks[blockIdx] = fileToMove.copy(empty = true, fileIdx = null)
+            this.blocks[blockIdx] = fileToMove.copy(fileIdx = -1)
 
             // find the first empty block where it fits
             val emptyBlockIdx = this.blocks.indexOfFirst { it.empty && it.length >= fileToMove.length }
             if (emptyBlockIdx != -1) {
                 val emptyBlock = this.blocks[emptyBlockIdx]
-                if (emptyBlock.length == fileToMove.length) {
-                    // Simplest case, just replace
-                    blocks[emptyBlockIdx] = fileToMove
+                val emptySpaceLeft = emptyBlock.length - fileToMove.length
+                val nextBlock = blocks[emptyBlockIdx + 1]
+                if (nextBlock.empty && emptySpaceLeft > 0) {
+                    nextBlock.length += emptySpaceLeft
                 } else {
-                    val emptySpaceLeft = emptyBlock.length - fileToMove.length
-                    val nextBlock = blocks[emptyBlockIdx + 1]
-                    if (nextBlock.empty) {
-                        nextBlock.length+=emptySpaceLeft
-                    } else {
-                        blocks[emptyBlockIdx] = fileToMove
-                        blocks.add(
-                            emptyBlockIdx + 1,
-                            Block(empty = true, fileIdx = fileToMove.fileIdx, length = emptySpaceLeft)
-                        )
-                    }
+                    blocks[emptyBlockIdx] = fileToMove
+                    if (emptySpaceLeft > 0) blocks.add(
+                        emptyBlockIdx + 1,
+                        Block.empty(length = emptySpaceLeft)
+                    )
                 }
             }  // No fitting empty block found. nop
         }
         return score(blocksToDisk(blocks))
     }
 
-    private fun blocksToDisk(blocks: MutableList<Block>): Array<Long?> {
-        val disk = Array<Long?>(size.toInt()) { null }
+    private fun blocksToDisk(blocks: MutableList<Block>): IntArray {
+        val disk = IntArray(size.toInt()) { EMPTY }
         var i = 0
-        blocks.forEachIndexed { idx, block ->
+        blocks.forEachIndexed { _, block ->
             repeat(block.length) {
                 if (block.empty) {
-                    disk[i] = null
+                    disk[i] = EMPTY
                 } else {
-                    disk[i] = block.fileIdx!!
+                    disk[i] = block.fileIdx
                 }
                 i++
             }
