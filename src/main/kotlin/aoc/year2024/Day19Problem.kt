9751fc016273595e714c78bcb8539ae05bf55842
Tomas Lööw <tomas.loow@mailbox.org>
2024-12-19 06:31:40 +0100

    Add 2024 day 19

---
+++ src/main/kotlin/aoc/year2024/Day19Problem.kt
@@ -0,0 +1,58 @@
+package aoc.year2024
+
+import DailyProblem
+import aoc.utils.*
+import java.util.HashMap
+import kotlin.time.ExperimentalTime
+
+class Day19Problem : DailyProblem<Long>() {
+
+    override val number = 19
+    override val year = 2024
+    override val name = "Linen Layout"
+
+    private lateinit var patterns: List<String>
+    private lateinit var goals: List<String>
+    private lateinit var counts: List<Long>
+
+    override fun commonParts() {
+        parseTwoBlocks(
+            getInputText(),
+            { parseOneLineOfSeparated(it, ::id, ", ") },
+            String::nonEmptyLines
+        ).let { (p, g) ->
+            patterns = p
+            goals = g
+        }
+
+        val cache: MutableMap<String, Long> = mutableMapOf("" to 1)
+        counts = goals.map { countWaysToMakeDesign(it, cache) } // Use this for both 1 and 2
+    }
+
+    /** Simple recursive count with a cache of already calculated substrings */
+    fun countWaysToMakeDesign(goal: String, cache: MutableMap<String, Long>): Long {
+        if (goal.isEmpty()) return 1
+        if (goal in cache) return cache[goal]!!
+        val prefixes: List<String> = patterns.filter { goal.startsWith(it) }
+        if (prefixes.isEmpty()) return 0
+        val res = prefixes.sumOf { countWaysToMakeDesign(goal.removePrefix(it), cache) }
+        cache[goal] = res
+        return res
+    }
+
+    override fun part1(): Long {
+        return counts.count { it != 0L }.toLong()
+    }
+
+    override fun part2(): Long {
+        return counts.map { it }.sum()
+    }
+}
+
+val day19Problem = Day19Problem()
+
+@OptIn(ExperimentalTime::class)
+fun main() {
+    day19Problem.testData = false
+    day19Problem.runBoth(50)
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -21,7 +21,8 @@ val events = mapOf(
         aoc.year2024.day15Problem,
         aoc.year2024.day16Problem,  //Slooooowwww.... :(
         aoc.year2024.day17Problem,
-        aoc.year2024.day18Problem
+        aoc.year2024.day18Problem,
+        aoc.year2024.day19Problem
     ),
     2023 to listOf(
         aoc.year2023.day1Problem,
