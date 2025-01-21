e0e20d67d6302d573ea011ab172d5edec8a3902c
Tomas Lööw <tomas.loow@sectra.com>
2025-01-21 14:59:16 +0100

    Add 2017 day 4

---
+++ src/main/kotlin/aoc/year2017/day04.kt
@@ -0,0 +1,46 @@
+package aoc.year2017
+
+import DailyProblem
+import kotlin.math.absoluteValue
+import kotlin.properties.Delegates
+import kotlin.time.ExperimentalTime
+import aoc.utils.extensionFunctions.nonEmptyLines
+import aoc.utils.geometry.Coord
+import aoc.utils.geometry.Direction
+
+class Day04Problem : DailyProblem<Long>() {
+
+    override val number = 4
+    override val year = 2017
+    override val name = "High-Entropy Passphrases"
+
+    private lateinit var lines : List<List<String>>
+
+    override fun commonParts() {
+        lines = getInputText().nonEmptyLines().map { line -> line.split(" ") }
+    }
+
+    override fun part1(): Long {
+        return lines.count { line ->
+            line.distinct().size == line.size
+        }.toLong()
+    }
+
+    override fun part2(): Long {
+        return lines.map { line -> line.map { it.sorted() } }.count { line ->
+            line.distinct().size == line.size
+        }.toLong()
+    }
+}
+
+fun String.sorted(): String {
+    return this.toList().sorted().toString()
+}
+
+val day04Problem = Day04Problem()
+
+@OptIn(ExperimentalTime::class)
+fun main() {
+    day04Problem.testData = false
+    day04Problem.runBoth(100)
+}
